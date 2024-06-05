c721dfcb47f62633b3a87296d71a3766cd71ed76
dokhacduong3 <dokhacduong3@gmail.com>
2024-06-05 10:55:56 +0700

    add_more_product

--- public/js/index.js
+++ public/js/index.js
@@ -7,7 +7,8 @@ document.addEventListener('click', (event) => {
         topMenu.classList.toggle('ct-topmenu-expanded');
     }
 });
-
+ //Lấy kích thước màn hình để ẩn menu khi màn hình lớn hơn 1024px
+ 
 window.addEventListener("resize", () => {
     if (window.innerWidth > 1024 && topMenu.classList.contains("ct-topmenu-expanded")) {
         topMenu.classList.toggle('hidden');
